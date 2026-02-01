89b1fa8dfb73e6499390c3d6f59c799653e599c8
LordShadd <real.davidwilson@proton.me>
2026-02-01 13:14:50 -0300

    🐛 fix(rakstar): atualizar macro de eventos e nomes de campos

--- crates/rakstar/src/events.rs
+++ crates/rakstar/src/events.rs
@@ -1,25 +1,50 @@
 pub mod internal;
 
 pub use internal::{
-    PRIORITY_HIGHEST, PRIORITY_FAIRLY_HIGH, PRIORITY_DEFAULT, 
-    PRIORITY_FAIRLY_LOW, PRIORITY_LOWEST,
-    PRIORITY_INTERNAL, PRIORITY_USER, PRIORITY_LOW
+    PRIORITY_DEFAULT, PRIORITY_FAIRLY_HIGH, PRIORITY_FAIRLY_LOW, PRIORITY_HIGHEST,
+    PRIORITY_INTERNAL, PRIORITY_LOW, PRIORITY_LOWEST, PRIORITY_USER,
 };
 
+use crate::{Player, Vehicle};
+
 pub trait EventHandler {
-    fn on_player_connect(&mut self, _player_id: i32) {}
-    fn on_player_disconnect(&mut self, _player_id: i32, _reason: i32) {}
-    fn on_player_spawn(&mut self, _player_id: i32) {}
-    fn on_player_death(&mut self, _player_id: i32, _killer_id: i32, _weapon: i32) {}
-    fn on_player_text(&mut self, _player_id: i32, _text: &str) -> bool { true }
-    fn on_player_command_text(&mut self, _player_id: i32, _command: &str) -> bool { true }
-    fn on_player_request_class(&mut self, _player_id: i32, _class_id: i32) -> bool { true }
-    fn on_player_request_spawn(&mut self, _player_id: i32) -> bool { true }
-    fn on_player_key_state_change(&mut self, _player_id: i32, _new_keys: i32, _old_keys: i32) {}
-    fn on_player_stream_in(&mut self, _player_id: i32, _for_player_id: i32) {}
-    fn on_player_stream_out(&mut self, _player_id: i32, _for_player_id: i32) {}
-    fn on_player_take_damage(&mut self, _player_id: i32, _issuer_id: i32, _amount: f32, _weapon: i32, _bodypart: i32) {}
-    fn on_player_give_damage(&mut self, _player_id: i32, _damaged_id: i32, _amount: f32, _weapon: i32, _bodypart: i32) {}
-    fn on_player_enter_vehicle(&mut self, _player_id: i32, _vehicle_id: i32, _is_passenger: bool) {}
-    fn on_player_exit_vehicle(&mut self, _player_id: i32, _vehicle_id: i32) {}
+    fn on_player_connect(&mut self, _player: Player) {}
+    fn on_player_disconnect(&mut self, _player: Player, _reason: i32) {}
+    fn on_player_spawn(&mut self, _player: Player) {}
+    fn on_player_death(&mut self, _player: Player, _killer: Player, _weapon: i32) {}
+    fn on_player_text(&mut self, _player: Player, _text: String) -> bool {
+        true
+    }
+    fn on_player_command_text(&mut self, _player: Player, _command: String) -> bool {
+        true
+    }
+    fn on_player_request_class(&mut self, _player: Player, _class_id: i32) -> bool {
+        true
+    }
+    fn on_player_request_spawn(&mut self, _player: Player) -> bool {
+        true
+    }
+    fn on_player_key_state_change(&mut self, _player: Player, _new_keys: i32, _old_keys: i32) {}
+    fn on_player_stream_in(&mut self, _player: Player, _for_player: Player) {}
+    fn on_player_stream_out(&mut self, _player: Player, _for_player: Player) {}
+    fn on_player_take_damage(
+        &mut self,
+        _player: Player,
+        _issuer: Player,
+        _amount: f32,
+        _weapon: i32,
+        _bodypart: i32,
+    ) {
+    }
+    fn on_player_give_damage(
+        &mut self,
+        _player: Player,
+        _damaged: Player,
+        _amount: f32,
+        _weapon: i32,
+        _bodypart: i32,
+    ) {
+    }
+    fn on_player_enter_vehicle(&mut self, _player: Player, vehicle: Vehicle, _is_passenger: bool) {}
+    fn on_player_exit_vehicle(&mut self, _player: Player, vehicle: Vehicle) {}
 }
--- crates/rakstar/src/macros.rs
+++ crates/rakstar/src/macros.rs
@@ -18,6 +18,52 @@ use std::sync::OnceLock;
 
 static API: OnceLock<bindings::api::OmpApi> = OnceLock::new();
 
+pub trait FromCEvent<S> {
+    fn from_c(source: S) -> Self;
+}
+
+impl FromCEvent<bindings::types::PlayerPtr> for crate::Player {
+    fn from_c(ptr: bindings::types::PlayerPtr) -> Self {
+        crate::Player::from_ptr(ptr)
+    }
+}
+
+impl FromCEvent<bindings::types::VehiclePtr> for crate::Vehicle {
+    fn from_c(ptr: bindings::types::VehiclePtr) -> Self {
+        crate::Vehicle::from_ptr(ptr)
+    }
+}
+
+impl FromCEvent<bindings::types::CAPIStringView> for String {
+    fn from_c(view: bindings::types::CAPIStringView) -> Self {
+        unsafe { view.to_string().unwrap_or_default() }
+    }
+}
+
+impl<T> FromCEvent<T> for T {
+    fn from_c(val: T) -> Self {
+        val
+    }
+}
+
+#[macro_export]
+macro_rules! handle_event {
+    ($comp:expr, $method:ident, $args_ty:ty, $( ::<$T:ty>($field:ident) ),* ) => {
+        unsafe extern "C" fn $method(raw_args: *mut $args_ty) -> bool {
+            if let Some(ref mut comp) = $comp {
+                use $crate::EventHandler;
+                let list = &*(*raw_args).list;
+                comp.$method(
+                   $(
+                       <$T as $crate::macros::FromCEvent<_>>::from_c(*list.$field)
+                   ),*
+                );
+            }
+            true
+        }
+    };
+}
+
 #[macro_export]
 macro_rules! entrypoint {
     ($component:ty, $name:expr, $version:expr) => {
@@ -30,12 +76,12 @@ macro_rules! entrypoint {
 
             let mut api_uninit = MaybeUninit::<$crate::macros::__OmpApi>::uninit();
             let api_ptr = api_uninit.as_mut_ptr();
-            
+
             if !$crate::macros::__initialize_capi(api_ptr) {
                 eprintln!("Failed to initialize open.mp C API");
                 return ::std::ptr::null_mut();
             }
-            
+
             let api = api_uninit.assume_init();
             let _ = $crate::macros::set_api(api);
 
@@ -49,186 +95,179 @@ macro_rules! entrypoint {
             };
 
             let name = CString::new($name).unwrap();
-            
-            unsafe extern "C" fn on_player_connect_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_connect(0);
-                }
-                true
-            }
 
-            unsafe extern "C" fn on_player_disconnect_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_disconnect(0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_connect,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerConnect>,
+                ::<$crate::Player>(player)
+            );
 
-            unsafe extern "C" fn on_player_spawn_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_spawn(0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_disconnect,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerDisconnect>,
+                ::<$crate::Player>(player),
+                ::<_>(reason)
+            );
 
-            unsafe extern "C" fn on_player_death_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_death(0, 0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_spawn,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerSpawn>,
+                ::<$crate::Player>(player)
+            );
 
-            unsafe extern "C" fn on_player_text_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    return comp.on_player_text(0, "");
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_death,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerDeath>,
+                ::<$crate::Player>(player),
+                ::<$crate::Player>(killer),
+                ::<_>(reason)
+            );
 
-            unsafe extern "C" fn on_player_command_text_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    return comp.on_player_command_text(0, "");
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_text,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerText>,
+                ::<$crate::Player>(player),
+                ::<String>(text)
+            );
 
-            unsafe extern "C" fn on_player_request_class_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    return comp.on_player_request_class(0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_command_text,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerCommandText>,
+                ::<$crate::Player>(player),
+                ::<String>(command)
+            );
 
-            unsafe extern "C" fn on_player_request_spawn_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    return comp.on_player_request_spawn(0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_request_class,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerRequestClass>,
+                ::<$crate::Player>(player),
+                ::<_>(classId)
+            );
 
-            unsafe extern "C" fn on_player_key_state_change_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_key_state_change(0, 0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_request_spawn,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerRequestSpawn>,
+                ::<$crate::Player>(player)
+            );
 
-            unsafe extern "C" fn on_player_stream_in_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_stream_in(0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_key_state_change,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerKeyStateChange>,
+                ::<$crate::Player>(player),
+                ::<_>(newKeys),
+                ::<_>(oldKeys)
+            );
 
-            unsafe extern "C" fn on_player_stream_out_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_stream_out(0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_stream_in,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerStreamIn>,
+                ::<$crate::Player>(player),
+                ::<$crate::Player>(forPlayer)
+            );
 
-            unsafe extern "C" fn on_player_take_damage_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_take_damage(0, 0, 0.0, 0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_stream_out,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerStreamOut>,
+                ::<$crate::Player>(player),
+                ::<$crate::Player>(forPlayer)
+            );
 
-            unsafe extern "C" fn on_player_give_damage_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_give_damage(0, 0, 0.0, 0, 0);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_take_damage,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerTakeDamage>,
+                ::<$crate::Player>(player),
+                ::<$crate::Player>(from),
+                ::<_>(amount),
+                ::<_>(weapon),
+                ::<_>(bodypart)
+            );
 
-            unsafe extern "C" fn on_player_enter_vehicle_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_enter_vehicle(0, 0, false);
-                }
-                true
-            }
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_give_damage,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerGiveDamage>,
+                ::<$crate::Player>(player),
+                ::<$crate::Player>(to),
+                ::<_>(amount),
+                ::<_>(weapon),
+                ::<_>(bodypart)
+            );
 
-            unsafe extern "C" fn on_player_exit_vehicle_callback() -> bool {
-                if let Some(ref mut comp) = COMPONENT {
-                    use $crate::EventHandler;
-                    comp.on_player_exit_vehicle(0, 0);
-                }
-                true
-            }
-            
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_enter_vehicle,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerEnterVehicle>,
+                ::<$crate::Player>(player),
+                ::<$crate::Vehicle>(vehicle),
+                ::<_>(passenger)
+            );
+
+            $crate::handle_event!(
+                COMPONENT,
+                on_player_exit_vehicle,
+                $crate::bindings::types::EventArgs<$crate::bindings::types::OnPlayerExitVehicle>,
+                ::<$crate::Player>(player),
+                ::<$crate::Vehicle>(vehicle)
+            );
+
+
             extern "C" fn on_ready() {
                 unsafe {
                     if let Some(ref mut comp) = COMPONENT {
                         comp.on_ready();
                     }
-                    
+
                     $crate::events::internal::register_internal_handlers();
-                    
-                    if let Some(api) = $crate::macros::get_api() {
-                        if let Some(add_handler) = api.event.add_handler {
-                            let event_name = CString::new("onPlayerConnect").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_connect_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerDisconnect").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_disconnect_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerSpawn").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_spawn_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerDeath").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_death_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerText").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_text_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerCommandText").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_command_text_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerRequestClass").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_request_class_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerRequestSpawn").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_request_spawn_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerKeyStateChange").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_key_state_change_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerStreamIn").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_stream_in_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerStreamOut").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_stream_out_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerTakeDamage").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_take_damage_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerGiveDamage").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_give_damage_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerEnterVehicle").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_enter_vehicle_callback as *const ::std::ffi::c_void));
-                            
-                            let event_name = CString::new("onPlayerExitVehicle").unwrap();
-                            add_handler(event_name.as_ptr(), $crate::events::PRIORITY_USER, ::std::mem::transmute(on_player_exit_vehicle_callback as *const ::std::ffi::c_void));
-                        }
-                    }
+
+                            match $crate::macros::get_api() {
+                                Some(api) => {
+                                    if let Some(add_handler) = api.event.add_handler {
+                                        macro_rules! register_event {
+                                            ($c_name:expr, $cb:ident) => {
+                                                let event_name = CString::new($c_name).unwrap();
+                                                add_handler(
+                                                    event_name.as_ptr(),
+                                                    $crate::events::PRIORITY_USER,
+                                                    ::std::mem::transmute($cb as *const ::std::ffi::c_void),
+                                                );
+                                            };
+                                        }
+
+                                        register_event!("onPlayerConnect", on_player_connect);
+                                        register_event!("onPlayerDisconnect", on_player_disconnect);
+                                        register_event!("onPlayerSpawn", on_player_spawn);
+                                        register_event!("onPlayerDeath", on_player_death);
+                                        register_event!("onPlayerText", on_player_text);
+                                        register_event!("onPlayerCommandText", on_player_command_text);
+                                        register_event!("onPlayerRequestClass", on_player_request_class);
+                                        register_event!("onPlayerRequestSpawn", on_player_request_spawn);
+                                        register_event!("onPlayerKeyStateChange", on_player_key_state_change);
+                                        register_event!("onPlayerStreamIn", on_player_stream_in);
+                                        register_event!("onPlayerStreamOut", on_player_stream_out);
+                                        register_event!("onPlayerTakeDamage", on_player_take_damage);
+                                        register_event!("onPlayerGiveDamage", on_player_give_damage);
+                                        register_event!("onPlayerEnterVehicle", on_player_enter_vehicle);
+                                        register_event!("onPlayerExitVehicle", on_player_exit_vehicle);
+                                    }
+                                }
+                                None => {}
+                            }
+
                 }
             }
-            
+
             extern "C" fn on_reset() {
                 unsafe {
                     if let Some(ref mut comp) = COMPONENT {
@@ -236,7 +275,7 @@ macro_rules! entrypoint {
                     }
                 }
             }
-            
+
             extern "C" fn on_free() {
                 unsafe {
                     if let Some(ref mut comp) = COMPONENT {
@@ -279,6 +318,6 @@ pub trait Component: Default {
 }
 
 #[doc(hidden)]
-pub use bindings::types::ComponentVersion as __ComponentVersion;
-#[doc(hidden)]
 pub use bindings::api::{OmpApi as __OmpApi, initialize_capi as __initialize_capi};
+#[doc(hidden)]
+pub use bindings::types::ComponentVersion as __ComponentVersion;
